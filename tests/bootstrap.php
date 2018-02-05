0d7056ccc5ef774f2f49ec35851cd3f155911d59
supersmile2009 <supersmile2009@gmail.com>
2018-02-05 16:36:25 +0200

    Fixed dev autoload

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,6 +7,5 @@ if ( ! file_exists(dirname(__DIR__).'/vendor') ) {
     die("\nDependencies must be installed using composer:\nSee http://getcomposer.org\n\n");
 }
 
-$loader = require_once dirname(__DIR__).'/vendor/autoload.php';
-$loader->setPsr4('Loco\\Tests\\Utils\\Swizzle\\', __DIR__.'/Tests' );
+require_once dirname(__DIR__).'/vendor/autoload.php';
 
