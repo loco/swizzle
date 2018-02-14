e15f0a2bb0376035838df1859ba64973e577de46
Tim Whitlock <r-bitbucket@timwhitlock.info>
2018-02-14 11:51:46 +0000

    Removed command class from example

--- example/loco.php
+++ example/loco.php
@@ -18,10 +18,6 @@ $builder->verbose( STDERR );
 // Register custom Guzzle response classes - such things are meaningless to Swagger.
 // These classes don't have have to exist in this runtime, they just go into the service definition.
 $builder->registerResponseClass('exportArchive', '\Loco\Http\Response\ZipResponse' );
-
-// Register custom Guzzle command class to use instead of default OperationCommand
-// Passing empty command name, so it is used for all.
-$builder->registerCommandClass( '', '\Loco\Http\LocoCommand' );
        
 // Now we're ready to build from a live endpoint
 // This must be a Valid Swagger JSON resource listing.
